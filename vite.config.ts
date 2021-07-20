4e718c4c08e3139c491385899ca3437a42fce50a
kmacoders <hungkids3003@gmail.com>
2021-07-21 01:32:42 +0700

    build: not hash on build

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,15 @@ import { root, srcPath } from './config/basePath'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`
+      }
+    }
+  },
   resolve: {
     alias: {
       '@@': root,
