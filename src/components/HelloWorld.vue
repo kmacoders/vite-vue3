101b1931a4b92d9aa00df4c72ae474348722c1fa
kmacoders <hungkids3003@gmail.com>
2021-07-20 22:11:41 +0700

    build: add eslint vue 3 || base airbnb ts

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,7 +3,12 @@
 
   <p>
     Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
+    <a
+      href="https://code.visualstudio.com/"
+      target="_blank"
+    >
+      VSCode
+    </a>
     +
     <a
       href="https://marketplace.visualstudio.com/items?itemName=octref.vetur"
@@ -12,7 +17,12 @@
       Vetur
     </a>
     or
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
+    <a
+      href="https://github.com/johnsoncodehk/volar"
+      target="_blank"
+    >
+      Volar
+    </a>
     (if using
     <code>&lt;script setup&gt;</code>)
   </p>
@@ -27,7 +37,12 @@
     <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
   </p>
 
-  <button type="button" @click="count++">count is: {{ count }}</button>
+  <button
+    type="button"
+    @click="count++"
+  >
+    count is: {{ count }}
+  </button>
   <p>
     Edit
     <code>components/HelloWorld.vue</code> to test hot module replacement.
@@ -35,20 +50,21 @@
 </template>
 
 <script lang="ts">
-import { ref, defineComponent } from 'vue'
+import { ref, defineComponent } from 'vue';
+
 export default defineComponent({
   name: 'HelloWorld',
   props: {
     msg: {
       type: String,
-      required: true
-    }
+      required: true,
+    },
   },
   setup: () => {
-    const count = ref(0)
-    return { count }
-  }
-})
+    const count = ref(0);
+    return { count };
+  },
+});
 </script>
 
 <style scoped>
