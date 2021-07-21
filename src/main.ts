a538c4eb2aabcba6d0b37ee246fd5d055ce9c5a7
Huwng <huwng@Neos-MacBook-Pro-1719.local>
2021-07-21 14:18:00 +0700

    build(env): add env

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import dotenv from 'dotenv';
 import { createApp } from 'vue';
 import App from './App.vue';
 import './styles/main.scss';
 
 createApp(App).mount('#app');
+console.log(import.meta.env);
---
+++ src/types/env.d.ts
@@ -0,0 +1,6 @@
+interface ImportMetaEnv {
+  VITE_NODE_ENV: string;
+  VITE_CDN_URL: string;
+  VITE_SUBMISSION_ENDPOINT_PROD: string;
+  VITE_ANALYSIS_ENDPOINT_PROD: string;
+}
