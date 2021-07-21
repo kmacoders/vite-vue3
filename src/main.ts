e2c56a0443ff7e65fb8c3baeb0e629e02acbf8c0
Huwng <huwng@Neos-MacBook-Pro-1719.local>
2021-07-21 14:49:54 +0700

    refactor: remove unused code

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-import dotenv from 'dotenv';
 import { createApp } from 'vue';
 import App from './App.vue';
 import './styles/main.scss';
 
 createApp(App).mount('#app');
-console.log(import.meta.env);
+// console.log(import.meta.env);
